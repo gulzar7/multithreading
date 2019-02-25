f38def17aef172e1fc6199b63f78ec2a0b543220
gitbootcamp <ahmad.gulzar8@gmail.com>
2019-02-26 00:23:45 +0530

    Add files via upload

---
+++ qus8.java
@@ -0,0 +1,59 @@
+
+
+import java.util.concurrent.*;
+
+class ExecuterServiceSchedulingTasks {
+
+	public static void main(String[] args){
+
+		ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+
+		executorService.schedule(new Callable() {
+
+			@Override
+			public Object call() throws Exception {
+				System.err.println("by Schedule()");
+				System.out.println("I took 1 second in execution ;-)");
+				return null;
+			}
+		},1,
+				TimeUnit.SECONDS);
+		executorService
+				.scheduleWithFixedDelay(new Runnable() {
+					                        @Override
+					                        public void run() {
+						                        try {
+							                        Thread.sleep(2000L);
+							                        System.out.println("ScheduleWithFixedDelay Scheduled Task to executed after fixed interval");
+						                        } catch (InterruptedException e) {
+							                        e.printStackTrace();
+						                        }
+
+					                        }
+				                        }
+						,
+						0,
+						1,
+						TimeUnit.SECONDS);
+
+		executorService
+				.scheduleAtFixedRate(new Runnable() {
+					                     @Override
+					                     public void run() {
+						                     try {
+							                     Thread.sleep(2000L);
+							                     System.out.println("ScheduleAtFixedRate Scheduled Task to executed after fixed interval");
+						                     } catch (InterruptedException e) {
+							                     e.printStackTrace();
+						                     }
+					                     }
+				                     }
+						,
+						0,
+						1,
+						TimeUnit.SECONDS);
+
+
+
+	}
+}
